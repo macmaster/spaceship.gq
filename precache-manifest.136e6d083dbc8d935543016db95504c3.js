d4bd80737bda16ad2446ed14e65085654b672be6
Ronald Macmaster <ronnymacmaster@gmail.com>
2019-06-19 01:18:09 -0400

    Updates

---
+++ precache-manifest.136e6d083dbc8d935543016db95504c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3b9e614dd7f82db2619bc8ad984027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b41d3e96b66ca304591",
+    "url": "/static/js/2.cbeb29db.chunk.js"
+  },
+  {
+    "revision": "ef908e138fffdb8c9fe3",
+    "url": "/static/js/main.b49f2ebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
